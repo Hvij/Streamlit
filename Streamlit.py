29ebbe872d7cfbcfb4036b148eb7335c7c5c7d8b
Hvij <harsh.vijayvargiya@brightlifecare.com>
2024-09-23 16:45:14 +0530

    Update Streamlit.py

--- Streamlit.py
+++ Streamlit.py
@@ -1,63 +1,59 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-import altair as alt
-
 
 # Set the page layout to wide
 st.set_page_config(layout="wide")
 
-alt.themes.enable("dark")
+# # CSS styling
+# st.markdown("""
+# <style>
 
+# [data-testid="block-container"] {
+#     padding-left: 2rem;
+#     padding-right: 2rem;
+#     padding-top: 1rem;
+#     padding-bottom: 0rem;
+#     margin-bottom: -7rem;
+# }
 
-# CSS styling
-st.markdown("""
-<style>
+# [data-testid="stVerticalBlock"] {
+#     padding-left: 0rem;
+#     padding-right: 0rem;
+# }
 
-[data-testid="block-container"] {
-    padding-left: 2rem;
-    padding-right: 2rem;
-    padding-top: 1rem;
-    padding-bottom: 0rem;
-    margin-bottom: -7rem;
-}
+# [data-testid="stMetric"] {
+#     background-color: #393939;
+#     text-align: center;
+#     padding: 15px 0;
+# }
 
-[data-testid="stVerticalBlock"] {
-    padding-left: 0rem;
-    padding-right: 0rem;
-}
+# [data-testid="stMetricLabel"] {
+#   display: flex;
+#   justify-content: center;
+#   align-items: center;
+# }
 
-[data-testid="stMetric"] {
-    background-color: #393939;
-    text-align: center;
-    padding: 15px 0;
-}
+# [data-testid="stMetricDeltaIcon-Up"] {
+#     position: relative;
+#     left: 38%;
+#     -webkit-transform: translateX(-50%);
+#     -ms-transform: translateX(-50%);
+#     transform: translateX(-50%);
+# }
 
-[data-testid="stMetricLabel"] {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
+# [data-testid="stMetricDeltaIcon-Down"] {
+#     position: relative;
+#     left: 38%;
+#     -webkit-transform: translateX(-50%);
+#     -ms-transform: translateX(-50%);
+#     transform: translateX(-50%);
+# }
 
-[data-testid="stMetricDeltaIcon-Up"] {
-    position: relative;
-    left: 38%;
-    -webkit-transform: translateX(-50%);
-    -ms-transform: translateX(-50%);
-    transform: translateX(-50%);
-}
+# </style>
+# """, unsafe_allow_html=True)
 
-[data-testid="stMetricDeltaIcon-Down"] {
-    position: relative;
-    left: 38%;
-    -webkit-transform: translateX(-50%);
-    -ms-transform: translateX(-50%);
-    transform: translateX(-50%);
-}
-
-</style>
-""", unsafe_allow_html=True)
-
+@st.cache_data(ttl=900)
 def get_data():
     df = pd.read_excel('Inventory Distribution model.xlsx','Base Data')
     return df
